cca42a44eec19cd5181809a1f2f097203ca3da34
Csaba Fulop <csafulop@gmail.com>
2022-01-22 20:06:35 +0100

    FEATURE | cliend-side basic init

--- stock-client/src/app/app.component.ts
+++ stock-client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { createWorker } from 'tesseract.js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stock-client';
+  myAngularxQrCode = ''; 
+  ocrResult = 'Recognizing...';
+  img = 'https://scontent-vie1-1.xx.fbcdn.net/v/t1.15752-9/271585143_3085011435079542_4117000986681090894_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_ohc=9g1L6mDTdDEAX-X85k6&_nc_ht=scontent-vie1-1.xx&oh=03_AVLDm27rxjIsqbiKJHvYhsh2QpiEek_RU7O5D3IsjDjC8Q&oe=6211A77C';
+
+  constructor() {
+    this.myAngularxQrCode = 'Your QR code data string';
+    this.doOCR();
+  }
+
+  scanSuccessHandler(event: any) {
+    console.log(event);
+  }
+
+  async doOCR() {
+    console.log("started");
+    const worker = createWorker({
+      logger: m => console.log(m),
+    });
+    console.log("started2");
+    await worker.load();
+    await worker.loadLanguage('hun');
+    await worker.initialize('hun');
+    const { data: { text } } = await worker.recognize(this.img);
+    this.ocrResult = text;
+    console.log(text);
+    await worker.terminate();
+  }
 }
--- stock-client/src/app/app.module.ts
+++ stock-client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { QRCodeModule } from 'angularx-qrcode';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    QRCodeModule,
+    ZXingScannerModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
